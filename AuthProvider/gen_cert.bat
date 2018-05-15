d4d9cef9241aa2d92fb06c9257076d4f665d3d02
kablaa <kablaa@hackmy.world>
2018-05-15 13:06:23 -0700

    ok I think this is good

--- AuthProvider/gen_cert.bat
+++ AuthProvider/gen_cert.bat
@@ -1,1 +1,1 @@
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout key.pem -out certificate.crt
